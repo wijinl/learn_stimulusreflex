656b401e1b61c5eb18e5855a0ae863550c85c75b
Wijin Lau <wijin.lau@oakda.io>
2020-06-04 03:18:42 +0800

    final

---
+++ app/controllers/nogoods_controller.rb
@@ -0,0 +1,2 @@
+class NogoodsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  root 'todos#index'
   resources :people
   resources :messages
-  root 'todos#index'
   resources :todos
+  resource :nogood
+  resource :note
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
